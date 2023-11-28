37cde3a1ec0ada48eef356e2f4e2914dbb7c0466
Kat Syddall <kat.syddall@gmail.com>
2023-11-28 15:56:34 +0100

    new event form complete, successfully redirects to show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,25 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save!
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :time, :category, :description, :venue_name,
+                                  :venue_address, :capacity, :price, :image_url)
+  end
 end
