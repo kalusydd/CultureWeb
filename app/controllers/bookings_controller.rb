1f478940498f0a4e3c4f58171d8700ef2144833f
Kat Syddall <kat.syddall@gmail.com>
2023-11-28 18:05:46 +0100

    i'm going button successfully creates booking. Almost complete, just need to finish dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,19 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new
-    @booking.user = current_user
     @booking.event = Event.find(params[:event_id])
-    if @booking.event.attendees < @booking.event.capacity
-      @booking.save!
+    @booking.user = current_user
+    if @booking.event.attendees.to_i < @booking.event.capacity.to_i
+      @booking.save
       redirect_to @booking.event, notice: "Woohoo! You're going to #{@booking.event.title} on #{@booking.event.date}!" # change redirect to dashboard
     else
-      "Sorry! This event is fully booked 🥲"
+      redirect_to @booking.event, alert: "Sorry! This event is fully booked 🥲"
     end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:event_id)
   end
 end
