29897143da74cb90b649ffabc498c3c7787b88bb
Louise Giraud <louisegiraud7@gmail.com>
2023-12-01 12:42:39 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,10 @@ class PagesController < ApplicationController
     @bookings_as_org = current_user.bookings_as_org
     @events_as_org = current_user.events
   end
+
+  def chat_dashboard
+    @bookings_as_attendee = current_user.bookings
+    @bookings_as_org = current_user.bookings_as_org
+    @events_as_org = current_user.events
+  end
 end
