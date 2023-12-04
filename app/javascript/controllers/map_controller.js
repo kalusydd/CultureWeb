662c59711354c705e7dd7ee1c19e28c165757f17
Louise Giraud <louisegiraud7@gmail.com>
2023-12-04 11:05:00 +0100

    pop up marker index page

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -20,7 +20,7 @@ export default class extends Controller {
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_event_html)
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_event_html || marker.info_window_index_html)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
