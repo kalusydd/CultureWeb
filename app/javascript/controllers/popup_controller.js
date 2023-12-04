bb438c049464629c8a83f549e4a5e770da1748ef
Pascal.Stodieck <stodieck.p@gmail.com>
2023-12-04 16:49:01 +0100

    typed

---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="popup"
+export default class extends Controller {
+  connect() {
+    function openForm() {
+      function openForm() {
+        document.getElementById("myForm").style.display = "block";
+      }
+
+      function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+      }
+  }
+
+}}
