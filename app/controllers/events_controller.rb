e129d15418b71adab3a47c6ce3aebd19c41b1793
Louise Giraud <louisegiraud7@gmail.com>
2023-12-04 10:16:44 +0100

    pop up marker show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,8 @@ class EventsController < ApplicationController
 
   def show
     @booking = Booking.new
-    @marker = [{ lat: @event.latitude, lng: @event.longitude }]
+    @marker = [{ lat: @event.latitude, lng: @event.longitude,
+    info_window_event_html: render_to_string(partial: "info_window_event", locals: {event: @event}) }]
   end
 
   def new
